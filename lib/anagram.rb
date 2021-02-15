1e3bdf3dfd7bd0accd2b0dc96feba03a826effe6
Lena Hou <lenahou2013@gmail.com>
2021-02-15 16:40:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,15 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
-   
+
 def initialize(word)
   @word = word
 end
 
-def match(word)
-  word.split(",")
-
+def match(word_array)
+  word_array.select do |word|
+    word.split("").sort == @word.sort
+  end
 end
 
 end
